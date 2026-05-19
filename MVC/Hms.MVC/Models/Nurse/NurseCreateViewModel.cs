bc3dc71a198069c00421a499f455bd99bea4895e
ShankarSingh175 <shankarsingh0175@gmail.com>
2026-05-19 12:19:58 +0530

    Altered NurseCreateViewModel.cs file

--- MVC/Hms.MVC/Models/Nurse/NurseCreateViewModel.cs
+++ MVC/Hms.MVC/Models/Nurse/NurseCreateViewModel.cs
@@ -1,12 +1,20 @@
-﻿namespace Hms.MVC.Models.Nurse
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hms.MVC.Models.Nurse
 {
     public class NurseCreateViewModel
     {
-
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
+        [RegularExpression("^[A-Za-z].*$", ErrorMessage = "Name must start with a letter")]
         public string Name { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Position is required")]
+        [StringLength(100, ErrorMessage = "Position cannot exceed 100 characters")]
         public string Position { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "SSN is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "ID cannot be negative")]
         public int Ssn { get; set; }
     }
 }
